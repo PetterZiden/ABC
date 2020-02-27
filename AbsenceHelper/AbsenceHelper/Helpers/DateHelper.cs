fdcf88e2fc11a66fed31a6487e8a7da559694c18
PetterZidén <pziden@gmail.com>
2020-02-27 19:16:21 +0100

    Think it works

---
+++ AbsenceHelper/AbsenceHelper/Helpers/DateHelper.cs
@@ -0,0 +1,35 @@
+﻿using AbsenceHelper.Models;
+
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AbsenceHelper.Helpers
+{
+    public static class DateHelper
+    {
+        public static bool DateChecker(IEnumerable<AbsenceData> list, int numberOfDays)
+        {
+            if (list.Count() < numberOfDays)
+            {
+                return false;
+            }
+
+            var counter = 1;
+
+            for (int i = 0; i < list.Count(); i++)
+            {
+                if (list.ElementAt(i).Date.AddDays(1) == list.ElementAt(i + 1).Date)
+                {
+                    counter++;
+                }
+                if (counter == numberOfDays)
+                {
+                    return true;
+                }
+
+            }
+
+            return false; ;
+        }
+    }
+}
--- AbsenceHelper/AbsenceHelper/Program.cs
+++ AbsenceHelper/AbsenceHelper/Program.cs
@@ -13,8 +13,12 @@ namespace AbsenceHelper
             // Retrieve and update CsvList
             var csvList = Helpers.AbsenceHelper.UpdateAbsenceFromXml(Deserializer.GetCsvList(), Deserializer.GetXmlLists());
 
+            Console.WriteLine(Environment.NewLine + "............");
+            Console.WriteLine("............" + Environment.NewLine);
+
             // Report 1
             Console.WriteLine($"- Vilka anställnings-id:n är frånvarande någon gång under mars månad med minst 85% frånvaro?");
+
             var report1 = csvList.Where(p => p.Percentage >= new decimal(0.85) && p.Date.Month == 3).Select(c => c.EmployeeId).Distinct();
 
             foreach (var employeeId in report1)
@@ -27,25 +31,23 @@ namespace AbsenceHelper
 
             // Report 2
             Console.WriteLine($"- Hur många anställda är frånvarande någon gång under mars med frånvarotyp A?");
+
             var report2 = csvList.Where(p => p.TypeName.Equals('A') && p.Date.Month == 3).Select(c => c.EmployeeId).Distinct().Count();
+
             Console.WriteLine($"    {report2} anställda var frånvarande under denna period" + Environment.NewLine);
 
             Console.WriteLine(Environment.NewLine + "............");
             Console.WriteLine("............" + Environment.NewLine);
 
-            // Funkar inte
             // Report 3
             Console.WriteLine($"- Hur många anställda har en sammanhängande frånvaro om minst 5 dagar under april oavsett typ och procent?");
-            var report3 = csvList.Where(p => p.Date.Month == 4);
 
-            Console.WriteLine("asdfasd");
-        }
+            var report3 = csvList.Where(p => p.Date.Month == 4).GroupBy(c => c.EmployeeId).Where(a => DateHelper.DateChecker(a.Select(b => b), 5)).Count();
 
-        // ??"???!?!?!??!
-        private int getDaterange(List<AbsenceData> list)
-        {
-            
-            return 0;
+            Console.WriteLine($"    {report3} anställda hade minst 5 dagar sammanhängande frånvaro under denna period" + Environment.NewLine);
+
+            Console.WriteLine(Environment.NewLine + "............");
+            Console.WriteLine("............" + Environment.NewLine);
         }
     }
 }
